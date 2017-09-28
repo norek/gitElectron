d4321b1cb3d09a227023f2bc2e27390bb9b0ad65
norek <norekzal@gmail.com>
2017-09-28 21:04:18 +0200

    init

---
+++ gitWebApp/main.js
@@ -0,0 +1,37 @@
+// ./main.js
+const { app, BrowserWindow } = require('electron')
+const path = require('path');
+const url = require('url');
+
+let win = null;
+require('electron-reload')(__dirname);
+app.on('ready', function () {
+
+    // Initialize the window to our specified dimensions
+    win = new BrowserWindow({ width: 1000,height: 800, darkTheme: 'true',fullscreen: 'true' });
+
+    // Specify entry point
+    win.loadURL('http://localhost:4200');
+
+    // Show dev tools
+    // Remove this line before distributing
+    win.webContents.openDevTools()
+
+    // Remove window once app is closed
+    win.on('closed', function () {
+        win = null;
+    });
+
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
