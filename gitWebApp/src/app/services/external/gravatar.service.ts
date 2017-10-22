4d1e85d4cd59454439879d3fe320c69413dc09b7
Michał Sakwa <saka1988@gmail.com>
2017-10-22 20:52:59 +0200

    Fixed the MD5 import.

--- gitWebApp/src/app/services/external/gravatar.service.ts
+++ gitWebApp/src/app/services/external/gravatar.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { Md5 } from 'ts-md5/dist/md5';
+import { Md5 } from 'md5';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
