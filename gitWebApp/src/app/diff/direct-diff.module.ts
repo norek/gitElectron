282f53877433c00bacb9383efa1a1320dece5687
Michał Sakwa <saka1988@gmail.com>
2017-11-07 21:25:55 +0100

    Added styling to diff view.

--- gitWebApp/src/app/diff/direct-diff.module.ts
+++ gitWebApp/src/app/diff/direct-diff.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { DirectDiffComponent } from './direct-diff.component';
-import { DirectDiffHunkListComponent } from './direct-diff/direct-diff-hunk-list.component';
-import { DirectDiffHunkComponent } from './direct-diff/direct-diff-hunk.component';
+import { DirectDiffHunkListComponent, DirectDiffHunkComponent, DirectDiffHunkLinesComponent } from './direct-diff/direct-diff-components';
 import { DiffService } from '../services/diff.service';
 
 @NgModule({
     imports: [SharedModule],
     exports: [],
-    declarations: [DirectDiffComponent, DirectDiffHunkComponent, DirectDiffHunkListComponent],
+    declarations: [DirectDiffComponent, DirectDiffHunkComponent, 
+                    DirectDiffHunkListComponent, DirectDiffHunkLinesComponent],
     providers: [DiffService],
     entryComponents: [DirectDiffComponent]
 })
---
+++ gitWebApp/src/app/diff/direct-diff/direct-diff-components.ts
@@ -0,0 +1,3 @@
+export { DirectDiffHunkListComponent } from "./direct-diff-hunk-list.component";
+export { DirectDiffHunkLinesComponent } from "./direct-diff-hunk-lines.component";
+export { DirectDiffHunkComponent } from "./direct-diff-hunk.component";
